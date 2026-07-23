fac5a2d4111139786017ece430eef4952ed69df1
itsarya99 <aryashukla5859@gmail.com>
2026-07-23 15:34:40 +0530

    task1

--- Lab1/Eventloop/task.js
+++ Lab1/Eventloop/task.js
@@ -1,19 +1,35 @@
-const f1=()=>{
-    console.log("f1");
+const f1 = () => {
+  console.log("f1 starts");
+  f2();
+  console.log("f1 running");
+  console.log("f1 end");
+};
+const f2 = () => {
+  console.log("f2 starts");
+  f3();
+  console.log("f2 running");
+  console.log("f2 ends");
+};
+const f3 = () => {
+  console.log("f3 starts");
+  console.log("f3 running");
+  console.log("f3 ends");
+};
+function main() {
+  console.log("main");
+  f1();
 
-};
-const f2=()=>{
-    console.log("f2");
-};
-const f3=()=>{
-    console.log("f3");
-};
-function main(){
-    console.log("main");
-    f1();
-    f2();
-    f3();
-    console.log("end main");
-
+  console.log("end main");
 }
 main();
+//synchronous call
+//java script is synchronous and single threaded
+//in asynchronous we use event loop to manage the call stack
+
+/*
+asynchronous using timers
+set time out
+set immediate
+process.nectTick
+setInternal
+*/
