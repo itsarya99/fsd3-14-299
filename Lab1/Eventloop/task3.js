045b39f6f55309730abbfc37dfe7e7939498aa73
itsarya99 <aryashukla5859@gmail.com>
2026-07-28 10:13:31 +0530

    Lab2 28 july

--- Lab1/Eventloop/task3.js
+++ Lab1/Eventloop/task3.js
@@ -3,3 +3,7 @@ setTimeout(() => console.log("2: setTimeout "), 0);
 setImmediate(() => console.log("3: nextTick"));
 process.nextTick(() => console.log("4: nextTick"));
 console.log("5: end");
+
+
+
+
