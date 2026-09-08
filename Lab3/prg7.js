b5d05c42d1c62b2ebb4aafc49f06ade1ac84aba0
itsarya99 <aryashukla5859@gmail.com>
2026-09-08 09:42:39 +0530

    prg6and 7

---
+++ Lab3/prg7.js
@@ -0,0 +1,10 @@
+import http from "http";
+import { readFile } from "fs/promises";
+
+const server = http.createServer(async (req, res) => {
+  res.write("Loading....");
+  const text = await readFile("big.txt");
+  res.end(text);
+});
+
+server.listen(3000, () => console.log("Server is running..."));
